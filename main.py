eae30c888146e0b9b9f74e2dabc0c1c2ef6d71b8
mic-ray <49364387+mic-ray@users.noreply.github.com>
2021-05-03 23:43:56 +0200

    Added fetching artists album info
    Album info:
    - The albums of the found artist (based on the input) are fetched
    - Only relevant info of each album (name, id & release date) is saved

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from auth import auth_manager
 # Create authorized Spotipy instance
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-# Search for given artist
+# Search ID of given artist
 artist = input("Name of the artist you want to search for: ")
 artist_search = sp.search(artist, type="artist")
-print(artist_search)
+
+# Note that the ID of the first artist in the search results is used
+artist_id = artist_search["artists"]["items"][0]["id"]
+
+# Fetch relevant info from the artists albums
+artist_albums = sp.artist_albums(
+    artist_id=artist_id, album_type="album")
+album_info = [
+    {"name": album['name'], "id":album['id'], "date":album['release_date']} for album in artist_albums["items"]]
+
+print(album_info)
