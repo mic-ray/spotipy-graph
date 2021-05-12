ab14e6f1c6d0ab337b32009e48c433ef7fae47b5
mic-ray <49364387+mic-ray@users.noreply.github.com>
2021-05-13 01:36:43 +0200

    Added calculation of avg track length
    Main:
    - Added a function to calculate the average track length for a given
    list of album tracks
    - The average length is added to each album of the fetched album info list

--- main.py
+++ main.py
@@ -3,4 +3,18 @@ import pprint
 
 artist = input("Name of the artist you want to search for: ")
 
-pprint.pprint(fetch_album_info(artist))
+
+album_info = fetch_album_info(artist)
+
+
+def calc_avg_length(album_tracks):
+    avg = 0
+    for track in album_tracks:
+        avg += track["duration"]
+    return avg//len(album_tracks)
+
+
+for index, album in enumerate(album_info):
+    album_info[index]["avg_track_length"] = calc_avg_length(album["tracks"])
+
+print(album_info)
