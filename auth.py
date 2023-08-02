63e5b31c97308839c05f3ac1177706fc753a20d1
mic-ray <49364387+mic-ray@users.noreply.github.com>
2023-08-02 03:33:44 +0200

    fix: fetch spotify secrets from environment
    Auth:
    - Instead of implicitly fetching spotify client ID and client secret
    from environment varibales automatically when authenticating,
    added statements to fetch the variables explicitly

--- auth.py
+++ auth.py
@@ -1,4 +1,7 @@
 from spotipy.oauth2 import SpotifyClientCredentials
+import os
 
 # Authorize using Client Credentials Auth flow
-auth_manager = SpotifyClientCredentials()
+client_id = os.environ.get("spo_client_id")
+client_secret = os.environ.get("spo_client_secret")
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
